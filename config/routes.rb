61598a4e72ff1b47901faa0eb68c131582d815d2
David Colby <davidlcolby@gmail.com>
2021-07-11 18:32:47 -0500

    Add appropriate roots for logged in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'dashboard#show'
+  authenticated :user do
+
+  end
+  devise_for :users,
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      password: 'forgot',
+      confirmation: 'confirm',
+      sign_up: 'sign_up',
+      sign_out: 'signout'
+    }
+
+  authenticated :user do
+    root to: 'dashboard#show', as: :user_root
+  end
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
   get 'dashboard/show'
 end
