bd433ba05ea5fadb434a546a378addedab759960
David Colby <davidlcolby@gmail.com>
2021-07-14 11:18:25 -0500

    Update application layout, add modal component, move flash messages to view component

--- spec/components/shared/top_nav_component_spec.rb
+++ spec/components/shared/top_nav_component_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Shared::TopNavComponent, type: :component do
       render_inline(Shared::TopNavComponent.new(user: User.new))
 
       expect(rendered_component).to have_text 'Rails 6 Starter'
-      expect(rendered_component).to have_link 'Sign out', href: '/users/sign_out'
+      expect(rendered_component).to have_link 'Sign out', href: '/signout'
     end
   end
 
@@ -14,8 +14,8 @@ RSpec.describe Shared::TopNavComponent, type: :component do
     it 'renders a navbar with sign in and sign up links' do
       render_inline(Shared::TopNavComponent.new(user: nil))
 
-      expect(rendered_component).to have_link 'Sign in', href: '/users/sign_in'
-      expect(rendered_component).to have_link 'Sign up', href: '/users/sign_up'
+      expect(rendered_component).to have_link 'Sign in', href: '/login'
+      expect(rendered_component).to have_link 'Sign up', href: '/sign_up'
     end
   end
 end
